c68738aa8fa40b6af492fec2ea6df06d08c7400c
Christian <cep812@gmail.com>
2026-09-16 21:34:11 -0400

    Added shape and bar chart.

--- src/main/java/org/example/csc311_gui_basics/HelloController.java
+++ src/main/java/org/example/csc311_gui_basics/HelloController.java
@@ -1,6 +1,8 @@
 package org.example.csc311_gui_basics;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -14,6 +16,9 @@ public class HelloController {
     private AnchorPane mainPane;
 
     @FXML
+    private BarChart<String, Number> barChart;
+
+    @FXML
     private void onHelloButtonClick() {
         System.out.println("Button clicked!");
     }
@@ -25,6 +30,20 @@ public class HelloController {
         mainPane.setStyle(
                 "-fx-background-color: " + toHex(color) + ";"
         );
+    }
+
+    @FXML
+    private void initialize() {
+
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Programming Languages");
+
+        series.getData().add(new XYChart.Data<>("Java", 8));
+        series.getData().add(new XYChart.Data<>("Python", 6));
+        series.getData().add(new XYChart.Data<>("C++", 4));
+        series.getData().add(new XYChart.Data<>("JavaScript", 7));
+
+        barChart.getData().add(series);
     }
 
     private String toHex(Color color) {
